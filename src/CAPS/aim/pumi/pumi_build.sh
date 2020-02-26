d3284b810426f62d1c8086cbb9a906abfa50e1f1
Tucker Babcock <tbabcock03@gmail.com>
2020-02-25 20:56:00 -0500

    need to build both shared and static pumi libraries

--- src/CAPS/aim/pumi/pumi_build.sh
+++ src/CAPS/aim/pumi/pumi_build.sh
@@ -20,10 +20,25 @@ if [[ -n "$EGADS_DIR" ]]; then
       -DSCOREC_ENABLE_CXX11=YES \
       -DENABLE_EGADS=ON \
       -DEGADS_DIR="$EGADS_DIR" \
+      -DBUILD_SHARED_LIBS=FALSE
+
+    cmake --build . -j 4 --target install
+
+    # For some reason I need to build shared and static libaries to link
+    # correctly on linux
+    cmake .. \
+      -DCMAKE_INSTALL_PREFIX="$PWD/install" \
+      -DSCOREC_EXTRA_C_FLAGS="-O0 -g -Wno-array-bounds -fPIC" \
+      -DSCOREC_EXTRA_CXX_FLAGS="-O0 -g -Wno-array-bounds -fPIC" \
+      -DCMAKE_C_COMPILER="mpicc" \
+      -DCMAKE_CXX_COMPILER="mpicxx" \
+      -DSCOREC_ENABLE_CXX11=YES \
+      -DENABLE_EGADS=ON \
+      -DEGADS_DIR="$EGADS_DIR" \
       -DBUILD_SHARED_LIBS=TRUE
 
     cmake --build . -j 4 --target install
-  
+
   else
     echo "PUMI_DIR not set correctly!"
   fi
