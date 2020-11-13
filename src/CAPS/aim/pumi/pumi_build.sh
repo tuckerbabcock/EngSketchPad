524ff422c0c528261eb5f004b69ee9583aa00102
Tucker Babcock <tbabcock03@gmail.com>
2020-11-13 13:29:56 -0500

    changed pumi build to just rebuild existing build with EGADS enabled, fixed link issue where previously on linux I thought I needed to build shared and static libraries. This means the config for pumi is unchanged when building this aim, with the exception of enabling EGADS

--- src/CAPS/aim/pumi/pumi_build.sh
+++ src/CAPS/aim/pumi/pumi_build.sh
@@ -5,35 +5,9 @@ PUMI_DIR=$2
 
 if [[ -n "$EGADS_DIR" ]]; then
   if [[ -n "$PUMI_DIR" ]]; then
-    cd $PUMI_DIR
 
-    mkdir -pv build
-
-    cd build
-
-    cmake .. \
-      -DCMAKE_INSTALL_PREFIX="$EGADS_DIR" \
-      -DCMAKE_C_COMPILER="mpicc" \
-      -DCMAKE_CXX_COMPILER="mpicxx" \
-      -DENABLE_EGADS=ON \
-      -DEGADS_DIR="$EGADS_DIR" \
-      -DBUILD_SHARED_LIBS=FALSE \
-      -DCMAKE_POSITION_INDEPENDENT_CODE=ON
-
-    cmake --build . -j 4 --target install
-
-    # For some reason I need to build shared and static libaries to link
-    # correctly on linux
-    cmake .. \
-      -DCMAKE_INSTALL_PREFIX="$EGADS_DIR" \
-      -DCMAKE_C_COMPILER="mpicc" \
-      -DCMAKE_CXX_COMPILER="mpicxx" \
-      -DENABLE_EGADS=ON \
-      -DEGADS_DIR="$EGADS_DIR" \
-      -DBUILD_SHARED_LIBS=TRUE \
-      -DCMAKE_POSITION_INDEPENDENT_CODE=ON
-
-    cmake --build . -j 4 --target install
+    cmake "$PUMI_DIR/build" -DENABLE_EGADS=ON -DEGADS_DIR="$EGADS_DIR"
+    cmake --build "$PUMI_DIR/build" --target install
 
   else
     echo "PUMI_DIR not set correctly!"
